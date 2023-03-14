df1ae54165059ba65d7820b03a821738cbb704f6
Anas Asimi <anas2020cr7@gmail.com>
2023-03-14 23:21:27 +0100

    finish select input

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,25 +1,29 @@
-import { useState , useRef } from 'react'
+import { useState, useRef } from 'react'
 import './Select.scss'
 
-let optionsToggler;
 function Select() {
+
   let [language, setLanguage] = useState('french')
   let options = useRef()
+
   let languages = ['french', 'arabic', 'english']
-  function optionsShow() {
-    options.current.classList.add('expanded')
+
+  function optionsToggler() {
+    options.current.classList.toggle('expanded')
   }
+
   function optionsHide() {
     options.current.classList.remove('expanded')
   }
+
   return (
-    <div className="select-container" onMouseEnter={optionsShow} onMouseLeave={optionsHide}>
+    <div className="select-container" onClick={optionsToggler} onMouseLeave={optionsHide}>
       <div className="selected">
         {language}
         <img src="/angle-down.svg" height={'20px'} />
       </div>
       <div className="options" ref={options} >
-        {languages.map((option) => (<p className='option' key={option} onClick={e => setLanguage(option)}>{option}</p>))}
+        {languages.map((option) => (<p className='option' key={option} onClick={() => { setLanguage(option) }}>{option}</p>))}
       </div>
     </div>
   )
