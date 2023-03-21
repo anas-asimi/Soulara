6aaa13e670024d0c0cb28aa7d725e8c0dfd7226b
Anas Asimi <anas2020cr7@gmail.com>
2023-03-21 16:09:07 +0000

    add google map to contact page

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -3,7 +3,7 @@ import "../sass/About.scss";
 function About() {
   return (
     <>
-      <main>
+      <main className="about">
         <h5>QUI SOMMES-NOUS ?</h5>
         <p className="big">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <p className="big">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import Input from "../components/Input";
+import "../sass/Contact.scss";
 
 function Contact() {
+
   return (
-    <div>Contact</div>
+    <main className="contact">
+      <form action="#">
+      </form>
+      <div className="map">
+        <iframe
+          src="https://www.google.com/maps/d/embed?mid=1IoAZovsEJ1PppYu9BwaY7ChrbQebnG0&ehbc=2E312F"
+        ></iframe>
+      </div>
+    </main>
   )
 }
 
 export default Contact
+
+
+
+{/* 
+you can edit the map window here :
+https://www.google.com/maps/d/u/0/edit?mid=1IoAZovsEJ1PppYu9BwaY7ChrbQebnG0&usp=sharing
+*/}
