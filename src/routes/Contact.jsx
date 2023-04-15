b01d1783e40f03908f9ceffb86274340d6c9839e
Anas Asimi <anas2020cr7@gmail.com>
2023-04-15 21:28:18 +0000

    improve map window and product card

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,32 +1,56 @@
+import { useEffect, useRef } from 'react';
 import FancyButton from "../components/FancyButton";
 import PageBanner from "../components/PageBanner";
 import "../sass/Contact.scss";
 
 function Contact() {
 
+  let mapDiv = useRef(null)
+
+  async function initMap() {
+    // The location of soulara
+    const position = { lat: 33.8986412, lng: -5.515229 };
+    // Request needed libraries.
+    const { Map } = await google.maps.importLibrary("maps");
+    const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+    // The map, centered at soulara
+    let map = new Map(mapDiv.current, {
+      zoom: 12,
+      center: position,
+      mapId: "DEMO_MAP_ID",
+    });
+    // The marker, positioned at Uluru
+    const marker = new AdvancedMarkerView({
+      map: map,
+      position: position,
+      title: "Soulara",
+    });
+  }
+
+  useEffect(() => {
+    initMap();
+  })
+
+
   return (
     <>
-      <PageBanner title={'CONTACTEZ-NOUS'}/>
+      <PageBanner title={'CONTACTEZ-NOUS'} />
       <main className="contact">
         <form action="#">
 
           <div className="input-wrapper">
-            <label htmlFor='name'>Name :</label>
             <input type="text" name='name' id='name' placeholder='enter your name' />
           </div>
 
           <div className="input-wrapper">
-            <label htmlFor='email'>Email :</label>
             <input type="email" name='email' id='email' placeholder='enter your email' />
 
           </div>
           <div className="input-wrapper">
-            <label htmlFor='subject'>Subject :</label>
             <input type="text" name='subject' id='subject' placeholder='enter your subject' />
 
           </div>
           <div className="input-wrapper">
-            <label htmlFor='message'>Message :</label>
             <textarea name="message" id="message" placeholder="enter your message"></textarea>
           </div>
 
@@ -35,11 +59,12 @@ function Contact() {
           </div>
 
         </form>
-        <div className="map">
+        <div id="map" ref={mapDiv}></div>
+        {/* <div id="map">
           <iframe
             src="https://www.google.com/maps/d/embed?mid=1IoAZovsEJ1PppYu9BwaY7ChrbQebnG0&ehbc=2E312F"
           ></iframe>
-        </div>
+        </div> */}
       </main></>
   )
 }
