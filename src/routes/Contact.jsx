e65f7dc16e29a4383f4ea274c72c9fb351ba675a
Anas Asimi <anas2020cr7@gmail.com>
2023-05-10 00:46:57 +0100

    add notifications for message form

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -6,20 +6,43 @@ import "../sass/Contact.scss";
 function Contact() {
 
   const form = useRef();
+  const succesMessage = useRef();
+  const failmessage = useRef();
+
+  let timeOut
+  function showMessage({ current }) {
+    clearTimeout(timeOut)
+    current.classList.add('hidden')
+    setTimeout(() => {
+      current.classList.remove('hidden')
+      timeOut = setTimeout(() => {
+        current.classList.add('hidden')
+      }, 2000)
+    })
+  }
 
   const sendEmail = (e) => {
     e.preventDefault();
+    // check in inputs valid
     if (form.current.name.value && form.current.email.value && form.current.message.value) {
+      // send message
       emailjs.sendForm('soulara', 'template_ypz5rsq', form.current, 'B9FGys9dlXIav8myu')
+        // in succes
         .then((result) => {
           form.current.reset()
-          console.log(result.text);
+          showMessage(succesMessage)
         })
+        // in fail
         .catch(error => {
-          console.log(error.text);
+          console.log(error);
+          showMessage(failmessage)
         });
     }
-    else console.log("invalid inputs");
+    // invalid inputs
+    else {
+      console.log("invalid inputs");
+      showMessage(failmessage)
+    }
   };
 
   return (
@@ -51,6 +74,20 @@ function Contact() {
           ></iframe>
         </div>
       </main>
+      <div ref={succesMessage} className="message hidden succes">
+        <div className="inner">
+          <img src="/check-circle.svg" height={24} />
+          <p>Message Sent</p>
+          <div className="loading-bar"></div>
+        </div>
+      </div>
+      <div ref={failmessage} className="message hidden fail">
+        <div className="inner">
+          <img src="/exclamation.svg" height={24} />
+          <p>Message Fail</p>
+          <div className="loading-bar"></div>
+        </div>
+      </div>
     </>
   )
 }
