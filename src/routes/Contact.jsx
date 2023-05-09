20c789a2483a6ca6249b3aaf23c00b49f29f0589
Anas Asimi <anas2020cr7@gmail.com>
2023-05-09 19:38:13 +0100

    add emailjs to handle contact form

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,35 +1,47 @@
-import FancyButton from "../components/FancyButton";
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import PageBanner from "../components/PageBanner";
 import "../sass/Contact.scss";
 
 function Contact() {
 
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    if (form.current.name.value && form.current.email.value && form.current.message.value) {
+      emailjs.sendForm('soulara', 'template_ypz5rsq', form.current, 'B9FGys9dlXIav8myu')
+        .then((result) => {
+          form.current.reset()
+          console.log(result.text);
+        })
+        .catch(error => {
+          console.log(error.text);
+        });
+    }
+    else console.log("invalid inputs");
+  };
+
   return (
     <>
       <PageBanner title={'CONTACTEZ-NOUS'} />
       <main className="contact">
-        <form action="#">
-
+        <form ref={form} onSubmit={sendEmail}>
           <div className="input-wrapper">
-            <input type="text" name='name' id='name' placeholder='enter your name' />
-          </div>
-
-          <div className="input-wrapper">
-            <input type="email" name='email' id='email' placeholder='enter your email' />
-
+            <input type="text" name='name' id='name' placeholder='Enter your name' />
           </div>
           <div className="input-wrapper">
-            <input type="text" name='subject' id='subject' placeholder='enter your subject' />
-
+            <input type="email" name='email' id='email' placeholder='Enter your email' />
           </div>
           <div className="input-wrapper">
-            <textarea name="message" id="message" placeholder="enter your message"></textarea>
+            <textarea name="message" id="message" placeholder="Enter your message"></textarea>
           </div>
-
           <div className="input-wrapper">
-            <FancyButton destination={"/"} text={'Send'} size={'regular'} />
+            <button type="submit" className='button regular'>
+              <p className='regular'>Send</p>
+              <img src="/angle-down-white.svg" height={'32px'} />
+            </button>
           </div>
-
         </form>
         <div id="map">
           <iframe
