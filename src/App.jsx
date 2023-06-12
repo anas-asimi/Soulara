2ed8c9a25a07e13c83149414ae75003177344d70
Anas Asimi <anas2020cr7@gmail.com>
2023-06-12 17:07:29 +0100

    add lazy load to routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,51 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "./routes/Home";
-import About from "./routes/About";
-import Contact from "./routes/Contact";
-import Products from "./routes/Products";
-import Services from "./routes/Services";
-import NoMatch from "./routes/NoMatch";
-
-import './sanitize.css' // reset css
+import Loading from "./components/Loading";
+import React, { Suspense } from "react";
+// lazy load
+const About = React.lazy(() => import('./routes/About'));
+const Contact = React.lazy(() => import('./routes/Contact'));
+const Products = React.lazy(() => import('./routes/Products'));
+const Services = React.lazy(() => import('./routes/Services'));
+const NoMatch = React.lazy(() => import('./routes/NoMatch'));
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<Layout />}>
-				<Route index element={<Home />} />
-				<Route path="about" element={<About />} />
-				<Route path="contact" element={<Contact />} />
-				<Route path="products" element={<Products />} />
-				<Route path="services" element={<Services />} />
-				<Route path="*" element={<NoMatch />} />
+				<Route index
+					element={<Home />} />
+				<Route path="about"
+					element={
+						<Suspense fallback={<Loading />}>
+							<About />
+						</Suspense>
+					} />
+				<Route path="contact"
+					element={
+						<Suspense fallback={<Loading />}>
+							<Contact />
+						</Suspense>
+					} />
+				<Route path="products"
+					element={
+						<Suspense fallback={<Loading />}>
+							<Products />
+						</Suspense>
+					} />
+				<Route path="services"
+					element={
+						<Suspense fallback={<Loading />}>
+							<Services />
+						</Suspense>
+					} />
+				<Route path="*"
+					element={
+						<Suspense fallback={<Loading />}>
+							<NoMatch />
+						</Suspense>
+					} />
 			</Route>
 		</Routes>
 	)
