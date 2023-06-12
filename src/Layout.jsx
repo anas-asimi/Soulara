f0729a2a544f502a3b5c58aef715988d75065413
Anas Asimi <anas2020cr7@gmail.com>
2023-06-12 17:08:05 +0100

    add width and height to img elements move loader to separate component move language state to the parent (Layout component)

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,34 +1,33 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation, Outlet, Link } from "react-router-dom";
 import FancyButton from "./components/FancyButton";
 import Select from './components/Select'
+import Loading from "./components/Loading";
+
+// styles
+import './sanitize.css' // reset css
 import './sass/Layout.scss'
 
 function Layout() {
 
+  let [language, setLanguage] = useState('french')
   const [isExpanded, setExpanded] = useState(false)
-  const loader = useRef(null)
   let location = useLocation();
   // function run every time route changed
   useEffect(() => {
     document.getElementById('root').scrollTo(0, 0)
   }, [location]);
-  // hide the loader after 2s
-  useEffect(() => {
-    let delay = 2000
-    setTimeout(() => { loader.current.style.display = 'none' }, delay)
-  }, []);
 
   return (
     <>
-      <div className="loading" ref={loader}><div className="cercle"></div></div>
+      <Loading delay={500}/>
       <header>
         <nav className={`mobile ${isExpanded ? 'expanded' : ''}`}>
           <div className="panel">
-            <a href="#" target="_blank"><img src="/social/facebook.svg" alt="facebook" height={32} /></a>
-            <a href="#" target="_blank"><img src="/social/whatsapp.svg" alt="whatsapp" height={32} /></a>
-            <a href="#" target="_blank"><img src="/social/youtube.svg" alt="youtube" height={32} /></a>
-            <Select isWhite={true} />
+            <a href="#" target="_blank"><img src="/social/facebook.svg" alt="facebook" height='32px' width='32px' /></a>
+            <a href="#" target="_blank"><img src="/social/whatsapp.svg" alt="whatsapp" height='32px' width='32px' /></a>
+            <a href="#" target="_blank"><img src="/social/youtube.svg" alt="youtube" height='32px' width='32px' /></a>
+            <Select isWhite={true} {...{ language, setLanguage }} />
           </div>
           <Link onClick={() => { setExpanded(false) }} to={'/'} >Accueil</Link>
           <Link onClick={() => { setExpanded(false) }} to={'about'} >About</Link>
@@ -38,7 +37,7 @@ function Layout() {
         </nav>
         <div className="bar">
           <div className="brand">
-            <img src="/brand.png" alt="Soulara" height={48} />
+            <img src="/brand.png" alt="Soulara" height='48px' width='38px' />
             <div className="text">
               <p>MANSOURI</p>
               <p className='small'>etude et travaux divers</p>
@@ -50,10 +49,10 @@ function Layout() {
             <div className='line-3'></div>
           </button>
           <div className="panel">
-            <a href="#" target="_blank"><img src="/social/facebook.svg" alt="facebook" height={32} /></a>
-            <a href="#" target="_blank"><img src="/social/whatsapp.svg" alt="whatsapp" height={32} /></a>
-            <a href="#" target="_blank"><img src="/social/youtube.svg" alt="youtube" height={32} /></a>
-            <Select />
+            <a href="#" target="_blank"><img src="/social/facebook.svg" alt="facebook" height='32px' width='32px' /></a>
+            <a href="#" target="_blank"><img src="/social/whatsapp.svg" alt="whatsapp" height='32px' width='32px' /></a>
+            <a href="#" target="_blank"><img src="/social/youtube.svg" alt="youtube" height='32px' width='32px' /></a>
+            <Select isWhite={false} {...{ language, setLanguage }} />
           </div>
         </div>
         <nav className='desktop'>
@@ -74,9 +73,9 @@ function Layout() {
           </div>
           <div className="right">
             <div className="social">
-              <a href="#" target="_blank"><img src="/social/facebook.svg" alt="facebook" height={32} /></a>
-              <a href="#" target="_blank"><img src="/social/whatsapp.svg" alt="whatsapp" height={32} /></a>
-              <a href="#" target="_blank"><img src="/social/youtube.svg" alt="youtube" height={32} /></a>
+              <a href="#" target="_blank"><img src="/social/facebook.svg" alt="facebook" height='32px' width='32px' /></a>
+              <a href="#" target="_blank"><img src="/social/whatsapp.svg" alt="whatsapp" height='32px' width='32px' /></a>
+              <a href="#" target="_blank"><img src="/social/youtube.svg" alt="youtube" height='32px' width='32px' /></a>
             </div>
             <p>0639990766</p>
             <p>I-B DIAR MENZH ET-3 AP27، Rue de Chefchaouen, Meknes 50000</p>
